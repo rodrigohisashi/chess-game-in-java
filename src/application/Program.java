ecc8745e7676b05240e4e48e9e40e38e43e3c83d
RHT <rodrigohisa@gmail.com>
2021-07-05 12:24:15 -0300

    Starting the project and printing the first board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import chess.ChessMatch;
+
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Projeto iniciado");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces()); 
+		
 	}
 }
