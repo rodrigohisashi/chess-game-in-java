09e90be212a238f41d4737d5ce25e9b8d39f7699
RHT <rodrigohisa@gmail.com>
2021-07-11 13:52:55 -0300

    Fixed BUG: Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -130,7 +129,7 @@ public class ChessMatch {
 				throw new IllegalStateException("There is no piece to be promoted");
 			}
 			if (!type.equals("B") && type.equals("N") && type.equals("R") && !type.equals("Q")) {
-				throw new InvalidParameterException("Invalid type for promotion!");	
+				return promoted;	
 			}
 			
 			Position pos = promoted.getChessPosition().toPosition();
@@ -378,7 +377,7 @@ public class ChessMatch {
 		placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
 		placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
 		placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
-		placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('h', 7, new Pawn(board, Color.WHITE, this));
 
 	}
 
